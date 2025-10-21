c2a2c9bd1ea73120ef2f932a3e09fcf89c97dfb1
margosulja <blinxduh@gmail.com>
2025-10-21 11:56:25 +0100

    removed "monaco-editor" from "manuelChunks" in "vite.config.ts" as it was causing issues with deployment.
    Signed-off-by: margosulja <blinxduh@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,29 @@
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()],
-	server: {
-		hmr: {
-			overlay: false
-		}
-	},
-	build: {
-		minify: 'esbuild', 
-		cssMinify: true,
-		rollupOptions: {
-			output: {
-				//code splitting for better caching
-				manualChunks: {
-					'monaco': ['monaco-editor'],//single vendor chunks for better caching
-					'lucide': ['lucide-svelte', '@lucide/svelte']
-				}
-			}
-		}
-	},
-	//ignore this
-	optimizeDeps: {
-		include: ['lucide-svelte', '@lucide/svelte']
-	}
+    plugins: [sveltekit(), tailwindcss()],
+    server: {
+        hmr: {
+            overlay: false,
+        },
+    },
+    build: {
+        minify: "esbuild",
+        cssMinify: true,
+        rollupOptions: {
+            output: {
+                //code splitting for better caching
+                manualChunks: {
+                    // 'monaco': ['monaco-editor'],//single vendor chunks for better caching
+                    lucide: ["lucide-svelte", "@lucide/svelte"],
+                },
+            },
+        },
+    },
+    //ignore this
+    optimizeDeps: {
+        include: ["lucide-svelte", "@lucide/svelte"],
+    },
 });
